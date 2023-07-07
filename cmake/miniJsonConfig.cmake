4fac31d6995a228d789a04a0662daafcf3aefa3b
Luc Meric <lhmeric@osmozis.com>
2023-07-07 19:05:44 +0200

    [CMAKE]: Added cmake integration

--- cmake/miniJsonConfig.cmake
+++ cmake/miniJsonConfig.cmake
@@ -1,22 +1,26 @@
-add_library(${LIBRARY_NAME}
-  ${SOURCES})
-
+add_library(${LIBRARY_NAME} SHARED ${SOURCES})
 add_library(${PROJECT_NAME}::${LIBRARY_NAME} 
   ALIAS ${LIBRARY_NAME})
 
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(JSON_C REQUIRED json-c)
 
-set_target_properties(${LIBRARY_NAME} PROPERTIES LINKER_LANGUAGE     CXX                )
-set_target_properties(${LIBRARY_NAME} PROPERTIES PUBLIC_HEADER       "${HEADERS_PUBLIC}")
-set_target_properties(${LIBRARY_NAME} PROPERTIES OUTPUT_NAME         "${LIBRARY_NAME}"  )
-set_target_properties(${LIBRARY_NAME} PROPERTIES SUFFIX              ".a"               )
-set_target_properties(${LIBRARY_NAME} PROPERTIES PREFIX              ""                 )
+set_target_properties(${LIBRARY_NAME} PROPERTIES LINK_FLAGS          "-Wl,-rpath,${CMAKE_INSTALL_PREFIX}/lib" )
+set_target_properties(${LIBRARY_NAME} PROPERTIES LINKER_LANGUAGE     CXX                                      )
+set_target_properties(${LIBRARY_NAME} PROPERTIES PUBLIC_HEADER       "${HEADERS_PUBLIC}"                      )
+set_target_properties(${LIBRARY_NAME} PROPERTIES OUTPUT_NAME         "${LIBRARY_NAME}"                        )
+set_target_properties(${LIBRARY_NAME} PROPERTIES SUFFIX              ".so"                                    )
+set_target_properties(${LIBRARY_NAME} PROPERTIES PREFIX              "lib"                                    )
 
 target_compile_definitions(
     ${LIBRARY_NAME} 
     PUBLIC
         "${PROJECT_NAME_UPPERCASE}_DEBUG=$<CONFIG:Debug>")
+
+target_link_directories(${LIBRARY_NAME}
+    PUBLIC
+        ${CMAKE_INSTALL_PREFIX}/lib
+)
 
 target_include_directories(
     ${LIBRARY_NAME} 
@@ -33,17 +37,13 @@ target_link_libraries(
 )
 
 install(
-    TARGETS              "${LIBRARY_NAME}"
-    EXPORT               "${TARGETS_EXPORT_NAME}"
-    LIBRARY DESTINATION  "${CMAKE_INSTALL_LIBDIR}"
-    ARCHIVE DESTINATION  "${CMAKE_INSTALL_LIBDIR}"
-    RUNTIME DESTINATION  "${CMAKE_INSTALL_BINDIR}"
-    INCLUDES DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
-)
-
-install(
-    FILES        ${HEADERS_PUBLIC}
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${LIBRARY_FOLDER}"
+    TARGETS                      "${LIBRARY_NAME}"
+    EXPORT                       "${TARGETS_EXPORT_NAME}"
+    LIBRARY         DESTINATION  "${CMAKE_INSTALL_LIBDIR}"
+    ARCHIVE         DESTINATION  "${CMAKE_INSTALL_LIBDIR}"
+    RUNTIME         DESTINATION  "${CMAKE_INSTALL_BINDIR}"
+    INCLUDES        DESTINATION  "${CMAKE_INSTALL_INCLUDEDIR}"
+    PUBLIC_HEADER   DESTINATION  "${CMAKE_INSTALL_INCLUDEDIR}/${LIBRARY_FOLDER}"
 )
 
 install(
